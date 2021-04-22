46a674a3dc30cc9f05864906975fa2d2e85f81fb
kmekdoud <karimmekdoud@gmail.com>
2021-04-22 13:11:10 -0400

    for naveen by karim and amar

--- src/main/java/solutionForNaveen/Naveen_AutomationLabs.java
+++ src/main/java/solutionForNaveen/Naveen_AutomationLabs.java
@@ -12,6 +12,9 @@ import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * BY KARIM and AMAR
+ */
 public class Naveen_AutomationLabs {
     static WebDriver driver;
     public static void main(String[] args) throws InterruptedException {
@@ -21,56 +24,60 @@ public class Naveen_AutomationLabs {
         driver.manage().window().maximize();
         driver.get("https://www.noon.com/uae-en/");
 
-        // List<String> resultList=Naveen_AutomationLabs_Function("Top picks in electronics");
-           List<String> resultList=Naveen_AutomationLabs_Function("Top picks in electronics");
-        // List<String> resultList=Naveen_AutomationLabs_Function("Save big on mobiles & tablets");
-        //List<String> resultList=Naveen_AutomationLabs_Function("Recommended For You");
-        System.out.println("The List of size is: "+ resultList.size());
-        for(String str:resultList){
-            System.out.println(str);
+        String [] SectionNames={"Top picks in electronics","Save big on mobiles & tablets","Recommended For You"};
+
+        for(String sectionName:SectionNames) {
+            System.out.println("**************************************************************************\n");
+            System.out.println("The list of items for the section >> "+sectionName+" << is:");
+
+            List<String> CarouselItems =Naveen_AutomationLabs_Function(sectionName);
+            System.out.println("List size is = " + CarouselItems.size());
+            for (String Items : CarouselItems) {
+                System.out.println(Items);
+            }
         }
         driver.quit();
 
     }
 
-    public static List<String> Naveen_AutomationLabs_Function(String locatorValue) throws InterruptedException {
-        boolean disable=false;
+    public static List<String> Naveen_AutomationLabs_Function(String SectionName) throws InterruptedException {
+        boolean nextArrowIsEnable=false;
         Actions ac = new Actions(driver);
-        List<String> ListElements = new ArrayList<>();
-        while (! disable) {
+        List<String> CarouselItemsText = new ArrayList<>();
+        while (! nextArrowIsEnable) {
             ac.sendKeys(Keys.PAGE_DOWN).build().perform();
             Thread.sleep(3000);
              try {
-                 String moveToElem_XPATH =String.format("//*[contains(text(),'%s')]//parent::div//parent::div/parent::div[1]/div[2]/div[1]/div[1]/div[1]/div/a/div/div[2]",locatorValue);
-                 //String moveToElem_XPATH ="//*[contains(text(),'"+locatorValue+"')]//parent::div//parent::div//parent::div[1]/div[2]/div[1]/div[1]/div[1]/div/a/div/div[2]";
-                 WebElement moveToElem= driver.findElement(By.xpath(moveToElem_XPATH));
-                 String swiper_button_next_XPATH =String.format("//*[contains(text(),'%s')]//parent::div//parent::div/parent::div[1]/div[2]/div[2]",locatorValue);
-                 WebElement swiper_button_next= driver.findElement(By.xpath(swiper_button_next_XPATH));
-                 ac.moveToElement(moveToElem).perform();
+                 String elementDescription_XPATH =String.format("//*[contains(text(),'%s')]//parent::div//parent::div/parent::div[1]/div[2]/div[1]/div[1]/div[1]/div/a/div/div[2]",SectionName);
+                 //String elementDescription_XPATH ="//*[contains(text(),'"+SectionName+"')]//parent::div//parent::div//parent::div[1]/div[2]/div[1]/div[1]/div[1]/div/a/div/div[2]";
+                 WebElement elementDescription = driver.findElement(By.xpath(elementDescription_XPATH));
+                 String nextArrow_XPATH =String.format("//*[contains(text(),'%s')]//parent::div//parent::div/parent::div[1]/div[2]/div[2]",SectionName);
+                 WebElement nextArrow= driver.findElement(By.xpath(nextArrow_XPATH));
+                 ac.moveToElement(elementDescription).perform();
                  int count=1;
                  try {
-                     while(swiper_button_next.isEnabled()) {
+                     while(nextArrow.isEnabled()) {
                          int i=0;
                          for(i=count; ;i++){
-                             String Element_XPATH =String.format("//*[contains(text(),'%s')]//parent::div//parent::div/parent::div[1]/div[2]/div[1]/div/div["+i+"]/div/a/div",locatorValue);
-                             WebElement Element =driver.findElement(By.xpath(Element_XPATH));
-                             if(Element.isDisplayed()){
-                                 String ElementTitle=Element.getAttribute("title");
-                                 ListElements.add(ElementTitle);
+                             String item_XPATH =String.format("//*[contains(text(),'%s')]//parent::div//parent::div/parent::div[1]/div[2]/div[1]/div/div["+i+"]/div/a/div",SectionName);
+                             WebElement item =driver.findElement(By.xpath(item_XPATH));
+                             if(item.isDisplayed()){
+                                 String ElementTitle=item.getAttribute("title");
+                                 CarouselItemsText.add(ElementTitle);
                                  count++;
                              }else{
-                                 disable=true;
+                                 nextArrowIsEnable=true;
                                  break;
                              }
                          }
-                         swiper_button_next.click();
+                         nextArrow.click();
                          Thread.sleep(2000);
                      }
                  }catch (Exception e){}
              }catch (Exception e){}
-             if(disable){ break;}
+             if(nextArrowIsEnable){ break;}
         }
-        Collections.sort(ListElements);
-        return ListElements;
+        Collections.sort(CarouselItemsText);
+        return CarouselItemsText;
     }
 }
